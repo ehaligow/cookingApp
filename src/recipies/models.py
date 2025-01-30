daa6babc70453d715f4ac0390f66d4906e473f96
ehaligow <emilahaligowska@gmail.com>
2025-01-30 14:53:07 +0100

    Add missing migrations and update Recipe model.

--- src/recipies/models.py
+++ src/recipies/models.py
@@ -13,6 +13,10 @@ class Recipe(models.Model):
     ingredients = models.TextField(max_length=500)
     instruction = models.TextField(max_length=500)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    slug = models.SlugField()
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return f'/{self.slug}/'
