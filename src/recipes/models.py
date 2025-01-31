8b85ce9e843716ccf4cc1dbfe6c184e7da82782a
ehaligow <emilahaligowska@gmail.com>
2025-01-31 14:55:30 +0100

    Add models and migrations.

--- src/recipes/models.py
+++ src/recipes/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Recipe(models.Model):
+    title = models.CharField(max_length=200)
+    ingredients = models.TextField(max_length=500)
+    instruction = models.TextField(max_length=500)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    slug = models.SlugField()
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return f'/{self.slug}/'
