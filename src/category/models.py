b6b1d6653b675453bf9a40d527ddbdde7f2b69f0
ehaligow <emilahaligowska@gmail.com>
2025-03-09 12:20:13 +0100

    Import category model from recipes app.

--- src/category/models.py
+++ src/category/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField()
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return f'/{self.slug}'
--- src/recipes/models.py
+++ src/recipes/models.py
@@ -1,11 +1,5 @@
 from django.db import models
-
-
-class Category(models.Model):
-    name = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.name
+from category.models import Category
 
 
 class Recipe(models.Model):
