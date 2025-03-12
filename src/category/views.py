c57985efec6b0677a0cd4a958d7f346fa62ba378
ehaligow <emilahaligowska@gmail.com>
2025-03-12 11:34:57 +0100

    GET Category queryset or POST a Category.

--- src/category/views.py
+++ src/category/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework.generics import ListCreateAPIView
+from .models import Category
+from .serializers import CategorySerializer
 
-# Create your views here.
+
+class CategoriesList(ListCreateAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
